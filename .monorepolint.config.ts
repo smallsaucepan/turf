5935ec6154d99ec6296b5d1ed4d0ea2f58e8fa5f
James Beard <james@smallsaucepan.com>
2023-10-04 22:47:23 +1100

    Add lerna caching (via nx). Simplistic setup so will need some tweaking. Have to add --npm-path to all npm-run-all calls so that lerna doesn't accidentally end up being used as the npm binary. https://github.com/mysticatea/npm-run-all/issues/218 and https://github.com/lerna/lerna/issues/1842 have more details.

--- .monorepolint.config.ts
+++ .monorepolint.config.ts
@@ -155,7 +155,7 @@ module.exports = {
         options: {
           scripts: {
             docs: "node ../../scripts/generate-readmes",
-            test: "npm-run-all test:*",
+            test: "npm-run-all --npm-path npm test:*",
           },
         },
         excludePackages: [MAIN_PACKAGE],
@@ -163,7 +163,7 @@ module.exports = {
       {
         options: {
           scripts: {
-            build: "npm-run-all build:*",
+            build: "npm-run-all --npm-path npm build:*",
             "build:js": "tsc",
             "build:es":
               'tsc --outDir dist/es --module esnext --declaration false && echo \'{"type":"module"}\' > dist/es/package.json',
